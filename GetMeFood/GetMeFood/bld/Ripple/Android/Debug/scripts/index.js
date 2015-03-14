35503a1250115fe3379209471b11cacc3d8b6229
unknown <nate.schultz@outlook.com>
2015-03-14 00:07:05 -0400

    added project

---
+++ GetMeFood/GetMeFood/bld/Ripple/Android/Debug/scripts/index.js
@@ -0,0 +1,36 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+
+})();
+
+//function studyClick() {
+//    window.location.replace('study.html');
+//}
+
+//jQuery(document).ready(function ($) {
+//    $(".feed_table_row").click(function () {
+//        window.location.replace('study.html');
+//    });
+//});
